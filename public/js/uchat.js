35056d562facf48eb4840bf20f9b6e5364732a0f
Rizwan <rizwandiwan749@gmail.com>
2020-11-28 11:57:08 +0530

    updated

--- public/js/uchat.js
+++ public/js/uchat.js
@@ -14,21 +14,16 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 const { c_user, secret } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
 const autoscroll = () => {
-    // New message element
     const $newMessage = $messages.lastElementChild
 
-    // Height of the new message
     const newMessageStyles = getComputedStyle($newMessage)
     const newMessageMargin = parseInt(newMessageStyles.marginBottom)
     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
 
-    // Visible height
     const visibleHeight = $messages.offsetHeight
 
-    // Height of messages container
     const containerHeight = $messages.scrollHeight
 
-    // How far have I scrolled?
     const scrollOffset = $messages.scrollTop + visibleHeight
 
     if (containerHeight - newMessageHeight <= scrollOffset) {
